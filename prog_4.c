2513b77c208a7110c0b51a4efc22288529f39157
Bidhan318 <bidhantimilsina318@gmail.com>
2026-02-06 21:06:09 +0545

    insert and delete node funcs added

--- prog_4.c
+++ prog_4.c
@@ -31,6 +31,48 @@ void insertAtEnd(Node** head, int data) {
     newNode->prev = temp;
 }
 
+// Insert after a given node
+void insertAfter(Node* prevNode, int data) {
+    if (prevNode == NULL) {
+        printf("Previous node cannot be NULL\n");
+        return;
+    }
+    
+    Node* newNode = createNode(data);
+    newNode->next = prevNode->next;
+    prevNode->next = newNode;
+    newNode->prev = prevNode;
+    
+    if (newNode->next != NULL) {
+        newNode->next->prev = newNode;
+    }
+}
+
+// Delete a specific node
+void deleteNode(Node** head, Node* delNode) {
+    if (*head == NULL || delNode == NULL) {
+        printf("Invalid operation\n");
+        return;
+    }
+    
+    // If node to be deleted is head
+    if (*head == delNode) {
+        *head = delNode->next;
+    }
+    
+    // Update next pointer of previous node
+    if (delNode->prev != NULL) {
+        delNode->prev->next = delNode->next;
+    }
+    
+    // Update prev pointer of next node
+    if (delNode->next != NULL) {
+        delNode->next->prev = delNode->prev;
+    }
+    
+    free(delNode);
+}
+
 void displayForward(Node* head) {
     if (head == NULL) {
         printf("List is empty\n");
@@ -48,18 +90,48 @@ void displayForward(Node* head) {
     printf(" -> NULL\n");
 }
 
-
 int main() {
     Node* head = NULL;
     
     printf("=== DOUBLY LINKED LIST ===\n\n");
     
+    // Create initial list
     insertAtEnd(&head, 10);
     insertAtEnd(&head, 20);
     insertAtEnd(&head, 30);
     insertAtEnd(&head, 40);
     insertAtEnd(&head, 50);
     
+    printf("Initial list:\n");
     displayForward(head);
+    
+    // Insert 25 after the second node (20)
+    printf("\nInserting 25 after node with value 20:\n");
+    insertAfter(head->next, 25);
+    displayForward(head);
+    
+    // Insert 35 after the node with value 30
+    printf("\nInserting 35 after node with value 30:\n");
+    Node* temp = head;
+    while (temp != NULL && temp->data != 30) {
+        temp = temp->next;
+    }
+    insertAfter(temp, 35);
+    displayForward(head);
+    
+    // Delete node with value 25
+    printf("\nDeleting node with value 25:\n");
+    temp = head;
+    while (temp != NULL && temp->data != 25) {
+        temp = temp->next;
+    }
+    deleteNode(&head, temp);
+    displayForward(head);
+    
+    // Delete head node
+    printf("\nDeleting head node (10):\n");
+    deleteNode(&head, head);
+    displayForward(head);
+    
     return 0;
 }
