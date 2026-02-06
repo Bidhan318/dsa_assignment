3204cadd54b08acd938a20a6e1e3e28b8b7406a8
Bidhan318 <bidhantimilsina318@gmail.com>
2026-02-06 19:53:51 +0545

    Q4 basic doubly linked list implementation:

---
+++ prog_4.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* prev;
+    struct Node* next;
+} Node;
+
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertAtEnd(Node** head, int data) {
+    Node* newNode = createNode(data);
+    
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    
+    Node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->prev = temp;
+}
+
+void displayForward(Node* head) {
+    if (head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+    
+    Node* temp = head;
+    printf("Forward: NULL <- ");
+    while (temp != NULL) {
+        printf("%d", temp->data);
+        if (temp->next != NULL)
+            printf(" <-> ");
+        temp = temp->next;
+    }
+    printf(" -> NULL\n");
+}
+
+
+int main() {
+    Node* head = NULL;
+    
+    printf("=== DOUBLY LINKED LIST ===\n\n");
+    
+    insertAtEnd(&head, 10);
+    insertAtEnd(&head, 20);
+    insertAtEnd(&head, 30);
+    insertAtEnd(&head, 40);
+    insertAtEnd(&head, 50);
+    
+    displayForward(head);
+    return 0;
+}
