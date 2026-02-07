4c87736b95927e7b4055417a494b5408e6bb9ee2
Bidhan318 <bidhantimilsina318@gmail.com>
2026-02-07 09:06:16 +0545

    DFS added

--- prog_5.c
+++ prog_5.c
@@ -3,6 +3,7 @@
 
 #define MAX 100
 
+// Queue structure for BFS
 typedef struct {
     int items[MAX];
     int front;
@@ -35,11 +36,13 @@ int dequeue(Queue* q) {
     return item;
 }
 
+// Add edge to undirected graph
 void addEdge(int adjMatrix[][MAX], int u, int v) {
     adjMatrix[u][v] = 1;
     adjMatrix[v][u] = 1;
 }
 
+// Display adjacency matrix
 void displayMatrix(int adjMatrix[][MAX], int numVertices) {
     printf("\nAdjacency Matrix:\n");
     printf("   ");
@@ -56,6 +59,7 @@ void displayMatrix(int adjMatrix[][MAX], int numVertices) {
     }
 }
 
+// BFS traversal using queue
 void BFS(int adjMatrix[][MAX], int numVertices, int startVertex) {
     Queue q;
     initQueue(&q);
@@ -71,6 +75,7 @@ void BFS(int adjMatrix[][MAX], int numVertices, int startVertex) {
         int currentVertex = dequeue(&q);
         printf("%d ", currentVertex);
         
+        // Visit all adjacent vertices
         for (int i = 0; i < numVertices; i++) {
             if (adjMatrix[currentVertex][i] == 1 && !visited[i]) {
                 visited[i] = 1;
@@ -81,9 +86,37 @@ void BFS(int adjMatrix[][MAX], int numVertices, int startVertex) {
     printf("\n");
 }
 
+// DFS helper function using recursion
+void DFSUtil(int adjMatrix[][MAX], int numVertices, int vertex, int visited[]) {
+    visited[vertex] = 1;
+    printf("%d ", vertex);
+    
+    // Visit all adjacent vertices
+    for (int i = 0; i < numVertices; i++) {
+        if (adjMatrix[vertex][i] == 1 && !visited[i]) {
+            DFSUtil(adjMatrix, numVertices, i, visited);
+        }
+    }
+}
+
+// DFS traversal using recursion
+void DFS(int adjMatrix[][MAX], int numVertices, int startVertex) {
+    int visited[MAX] = {0};
+    
+    printf("DFS Traversal starting from vertex %d: ", startVertex);
+    DFSUtil(adjMatrix, numVertices, startVertex, visited);
+    printf("\n");
+}
+
 int main() {
     int adjMatrix[MAX][MAX] = {0};
     int numVertices = 6;
+    
+    /* Example Graph:
+           0 --- 1 --- 2
+           |     |
+           3 --- 4 --- 5
+    */
     
     addEdge(adjMatrix, 0, 1);
     addEdge(adjMatrix, 0, 3);
@@ -97,6 +130,10 @@ int main() {
     printf("\n");
     BFS(adjMatrix, numVertices, 0);
     BFS(adjMatrix, numVertices, 2);
+    
+    printf("\n");
+    DFS(adjMatrix, numVertices, 0);
+    DFS(adjMatrix, numVertices, 2);
     
     return 0;
 }
