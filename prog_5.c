cba71b935800c9e86ac6cb076290a669bf83011e
Bidhan318 <bidhantimilsina318@gmail.com>
2026-02-07 08:50:04 +0545

    BFS implement

--- prog_5.c
+++ prog_5.c
@@ -35,17 +35,68 @@ int dequeue(Queue* q) {
     return item;
 }
 
-int main() {
+void addEdge(int adjMatrix[][MAX], int u, int v) {
+    adjMatrix[u][v] = 1;
+    adjMatrix[v][u] = 1;
+}
+
+void displayMatrix(int adjMatrix[][MAX], int numVertices) {
+    printf("\nAdjacency Matrix:\n");
+    printf("   ");
+    for (int i = 0; i < numVertices; i++)
+        printf("%d ", i);
+    printf("\n");
+    
+    for (int i = 0; i < numVertices; i++) {
+        printf("%d: ", i);
+        for (int j = 0; j < numVertices; j++) {
+            printf("%d ", adjMatrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void BFS(int adjMatrix[][MAX], int numVertices, int startVertex) {
     Queue q;
     initQueue(&q);
     
-    enqueue(&q, 10);
-    enqueue(&q, 20);
-    enqueue(&q, 30);
+    int visited[MAX] = {0};
     
-    printf("%d\n", dequeue(&q));
-    printf("%d\n", dequeue(&q));
-    printf("%d\n", dequeue(&q));
+    visited[startVertex] = 1;
+    enqueue(&q, startVertex);
+    
+    printf("BFS Traversal starting from vertex %d: ", startVertex);
+    
+    while (!isEmpty(&q)) {
+        int currentVertex = dequeue(&q);
+        printf("%d ", currentVertex);
+        
+        for (int i = 0; i < numVertices; i++) {
+            if (adjMatrix[currentVertex][i] == 1 && !visited[i]) {
+                visited[i] = 1;
+                enqueue(&q, i);
+            }
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    int adjMatrix[MAX][MAX] = {0};
+    int numVertices = 6;
+    
+    addEdge(adjMatrix, 0, 1);
+    addEdge(adjMatrix, 0, 3);
+    addEdge(adjMatrix, 1, 2);
+    addEdge(adjMatrix, 1, 4);
+    addEdge(adjMatrix, 3, 4);
+    addEdge(adjMatrix, 4, 5);
+    
+    displayMatrix(adjMatrix, numVertices);
+    
+    printf("\n");
+    BFS(adjMatrix, numVertices, 0);
+    BFS(adjMatrix, numVertices, 2);
     
     return 0;
 }
